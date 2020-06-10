8c15c4b4ec5b9342f462d67677f128de252ef8fb
Tomohiro Yasukura <t.yasukura0131@gmail.com>
2020-06-10 17:39:11 +0700

    ・mp > 0 を mp >= 0 へ修正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -25,7 +25,7 @@ public class Boss
     public void Magic()
     {
         this.mp -= 5;
-        if (mp > 0){
+        if (mp >= 0){
             Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
         } else {
             Debug.Log("MPが足りないため魔法が使えない。");
