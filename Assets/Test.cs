96734b649fb9c7e51b716e11014c2af86ef48679
Tomohiro Yasukura <t.yasukura0131@gmail.com>
2020-06-11 21:25:29 +0700

    MP が 0 以下にならないよう修正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -24,8 +24,8 @@ public class Boss
 
     public void Magic()
     {
-        this.mp -= 5;
-        if (mp >= 0){
+        if (mp >= 5){
+            this.mp -= 5;
             Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
         } else {
             Debug.Log("MPが足りないため魔法が使えない。");
